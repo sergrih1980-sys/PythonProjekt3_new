34e80ae01a5a5025e7bfb7cba1155117251efbc7
sergrih1980-sys <sergrih1980@gmail.com>
2026-01-04 19:55:05 +0300

    Исправление ошибок

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,10 @@ import pytest
 
 
 @pytest.fixture
-def symbol() -> None:
+def symbol() -> str:
     return "**3456"
 
 
 @pytest.fixture
-def account_empty() -> None:
+def account_empty() -> str:
     return "**"
